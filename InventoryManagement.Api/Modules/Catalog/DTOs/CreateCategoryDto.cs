04a8d749ecfb084d47a365f891f561d4d86cad5c
Inaros Lemuel <infromke@gmail.com>
2026-05-26 06:49:35 -0300

    fix: max length for description field in CreateCategoryDto should be 255 instead of 250

--- InventoryManagement.Api/Modules/Catalog/DTOs/CreateCategoryDto.cs
+++ InventoryManagement.Api/Modules/Catalog/DTOs/CreateCategoryDto.cs
@@ -7,7 +7,7 @@ namespace InventoryManagement.Api.Modules.Catalog.DTOs
         [MaxLength(100, ErrorMessage = "Name cannot exceed 100 characters")]
         string Name,
 
-        [MaxLength(250, ErrorMessage = "Description cannot exceed 250 characters")]
+        [MaxLength(255, ErrorMessage = "Description cannot exceed 255 characters")]
         string Description
     );
 }
