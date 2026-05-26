e9cc80bd175e632f3cf7de7d95a21dfa6409f7f5
Inaros Lemuel <infromke@gmail.com>
2026-05-26 07:27:48 -0300

    feat: create IProductService interface to decouple product operations

---
+++ InventoryManagement.Api/Modules/Catalog/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagement.Api.Modules.Catalog.DTOs;
+
+namespace InventoryManagement.Api.Modules.Catalog.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductResponseDto>> GetAllAsync();
+        Task<ProductResponseDto?> GetByIdAsync(int id);
+        Task<ProductResponseDto> CreateAsync(CreateProductDto dto);
+    }
+}
