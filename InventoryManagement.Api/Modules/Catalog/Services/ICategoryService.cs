cb34cc40bef69ec48ab823e3d6500bdf3982c437
Inaros Lemuel <infromke@gmail.com>
2026-05-25 23:18:05 -0300

    feat: create ICategoryService interface to decouple controller from business logic

---
+++ InventoryManagement.Api/Modules/Catalog/Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagement.Api.Modules.Catalog.DTOs;
+
+namespace InventoryManagement.Api.Modules.Catalog.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponseDto>> GetAllAsync();
+        Task<CategoryResponseDto?> GetByIdAsync(int id);
+        Task<CategoryResponseDto> CreateAsync(CreateCategoryDto dto);
+    }
+}
