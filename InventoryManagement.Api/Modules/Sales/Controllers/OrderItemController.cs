24d02879dbfaddacc5a3af002e2819d877e53253
Inaros Lemuel <infromke@gmail.com>
2026-05-28 19:41:38 -0300

    feat: implement controller endpoints for OrderItem with HttpGet actions

---
+++ InventoryManagement.Api/Modules/Sales/Controllers/OrderItemController.cs
@@ -0,0 +1,34 @@
+﻿using InventoryManagement.Api.Modules.Sales.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryManagement.Api.Modules.Sales.Controllers
+{
+    [ApiController]
+    [Route("api/sales/order-items")]
+    public class OrderItemController : ControllerBase
+    {
+        public readonly IOrderItemService _orderItemService;
+
+        public OrderItemController(IOrderItemService orderItemService)
+        {
+            _orderItemService = orderItemService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var orderItems = await _orderItemService.GetAllAsync();
+            return Ok(orderItems);
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var orderItem = await _orderItemService.GetByIdAsync(id);
+
+            if (orderItem == null) return NotFound(new {error = "Item not found"});
+
+            return Ok(orderItem);
+        }
+    }
+}
